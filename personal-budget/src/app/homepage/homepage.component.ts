09e83d6d0006158fbfd65cb26c476c22ba62633b
unknown <naraharikrishna007@gmail.com>
2023-09-24 10:17:57 +0530

    moved files to angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js/auto';
+
+
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit{
+
+  public dataSource:any={
+    datasets: [
+      {
+    data: [],
+    backgroundColor: [
+      '#ffcd56',
+      '#ff6384',
+      '#36a2eb',
+      '#fd6b19',
+      "#000080",
+      "#800080",
+      "#808080",
+      "#a52a2a"
+    ]
+  }
+],
+    labels: []
+  }
+
+  constructor(private http: HttpClient){
+    const el=document.getElementById('myChart');
+    console.log('Is my chart there?', el);
+
+  }
+
+  ngOnInit(): void {
+    const el=document.getElementById('myChart');
+    console.log('Is my chart there 2?', el);
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res: any)=> {
+
+      for(var i = 0; i < res.myBudget.length; i++ ){
+        this.dataSource.datasets[0].data[i]=res.myBudget[i].budget;
+        this.dataSource.labels[i]=res.myBudget[i].title;
+
+    }
+    this.createChart();
+    });
+
+  }
+
+  createChart() {
+    //var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx = <HTMLCanvasElement>document.getElementById('myChart');
+    var myPieChart=new Chart(ctx , {
+      type:'pie',
+      data: this.dataSource
+    });
+  };
+
+}
